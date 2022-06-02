bf0ca2583134b7806800053c1bd82cfa1981c0d0
ghazie754 <67240493+ghazie754@users.noreply.github.com>
2022-06-02 00:25:06 -0400

    shhh

--- main.py
+++ main.py
@@ -27,15 +27,7 @@ def undate(screen, cells, size , with_porgress=False):
                     color = COLOR_ALIVE_NEXT
         else:
             if alive == 3:
-                update_cells[row, col] = 2
-                if with_porgress:
-                    color = COLOR_ALIVE_NEXT
-            if alive == 9:
                 update_cells[row, col] = 1
-                if with_porgress:
-                    color = COLOR_ALIVE_NEXT
-            if alive == 8:
-                update_cells[row, col] = 3
                 if with_porgress:
                     color = COLOR_ALIVE_NEXT
         pygame.draw.rect(screen, color, (col * size, row * size, size - 1, size - 1))
@@ -73,13 +65,6 @@ def main():
                 undate(screen, cells, cellsize)
                 pygame.display.update()
         
-        for W in range(0, int(wow/10)):
-            for H in range(0, int(hi/10)):
-                if cells[W, 0] == 1 or cells[0, H] == 1 :
-                    cellscells[w,h] = 0
-                if cells[W, 119] == 1 or cells[79, H] == 1 :
-                    cells[W, HEIGHT] = 0
-                    cells[WIDTH, H] = 0
         screen.fill(COLOR_GRID)
         
         if run:
